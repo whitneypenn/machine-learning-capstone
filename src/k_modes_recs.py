14b3f109d775a2a0d8f7f5751a653642e16a6370
Whitney <whitneypenn15@gmail.com>
2018-07-31 19:11:05 -0600

    things are working

--- src/k_modes_recs.py
+++ src/k_modes_recs.py
@@ -21,15 +21,17 @@ data_to_cluster = data[['Project Resource Category',
 kproto = KModes(n_clusters=num_clusters, init='random', verbose=True)
 # here you use the unsclaed data and tell the model which columns are categorical
 # and which ones are numerical
-cluster_obj = kproto.fit(data)
+cluster_obj = kproto.fit(data_to_cluster)
 labels = cluster_obj.labels_
+print(cluster_obj.cluster_centroids_)
+
 
 # Print 10 titles in each cluster
-titles = data['Project Title'].values
-for i in range(num_clusters):
-    cluster = np.arange(0, data.shape[0])[labels == i]
-    sample_titles = cluster
-    print("cluster {}, {} samples:".format(i, len(cluster)))
-    cluster_titles = [titles[title] for title in sample_titles]
-    print(cluster_titles)
-    print(' ')
+# titles = data['Project Title'].values
+# for i in range(num_clusters):
+#     cluster = np.arange(0, data.shape[0])[labels == i]
+#     sample_titles = cluster
+#     print("cluster {}, {} samples:".format(i, len(cluster)))
+#     cluster_titles = [titles[title] for title in sample_titles]
+#     print(cluster_titles)
+#     print(' ')
