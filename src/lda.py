0b0596102344d99408d4a8c6f458f07df43fc9a6
Whitney <whitneypenn15@gmail.com>
2018-08-02 09:40:11 -0600

    make lda updates

--- src/lda.py
+++ src/lda.py
@@ -4,19 +4,28 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation, NMF
 from sklearn.model_selection import cross_validate
 import matplotlib.pyplot as plt
+from sklearn.feature_extraction import text
 
-def print_top_words(model, feature_names, n_top_words):
-    for topic_idx, topic in enumerate(model.components_):
-        print("Topic #%d:" % topic_idx)
-        print(" ".join([feature_names[i]
-            for i in topic.argsort()[:-n_top_words - 1:-1]]))
-    print()
+def get_top_words(model, feature_names, n_top_words):
+    topic_word_probs = []
+    distr = (model.components_ / model.components_.sum(axis=1)[:, np.newaxis])
+    #for topic_idx, topic in enumerate(model.components_):
+    for topic_idx, topic in enumerate(distr):
+        word_probs = []
+        idx = 0
+        for i in topic.argsort()[:-n_top_words - 1:-1]:
+            word = feature_names[i]
+            prob = topic[i]
+            word_probs.append((word, prob))
+        topic_word_probs.append(word_probs)
 
-def plot_perplexity(topics_list, train_perplexity_list, test_perplexity_list, best_count=None, file_name=None):
+    return topic_word_probs
+
+
+def plot_perplexity(topics_list, perplexity_list, best_count=None, file_name=None):
     fig, ax = plt.subplots()
     plt.title('Topics vs Perplexity')
-    plt.plot(topics_list, train_perplexity_list, linestyle='--', marker='o', label='Train Perplexity')
-    #plt.plot(topics_list, test_perplexity_list, linestyle='--', marker='o', label='Test Perplexity')
+    plt.plot(topics_list, perplexity_list, linestyle='--', marker='o', label='Train Perplexity')
     if best_count:
         plt.axvline(x=best_count, color='black', linestyle='--', label='Best Number of Clusters: {}'.format(best_count))
     plt.xlabel('Number of Topics')
@@ -26,66 +35,84 @@ def plot_perplexity(topics_list, train_perplexity_list, test_perplexity_list, be
         plt.savefig('images/{}.png'.format(file_name))
     plt.show()
 
+def test_perplexities(range_to_test, topic_frequency):
+    perplexities = []
+    for n in range_to_test:
+        print('working on {} topics.'.format(n))
 
-#input data
+        lda = LatentDirichletAllocation(n_components=n, max_iter=5,
+                                        learning_method='online',
+                                        learning_offset=50.,
+                                        random_state=0)
+
+        lda.fit(topic_frequency)
+
+        perplexities.append(lda.perplexity(topic_frequency))
+
+    plot_perplexity(range_to_test, perplexities)
+
+def plot_top_words(num_rows, num_cols, top_words_list, image_name=None):
+    fig, axs = plt.subplots(num_rows, num_cols, figsize=(10,3))
+    plt.subplots_adjust(left=.125, bottom=.1, right=.9, top=.9,
+                    wspace=.8, hspace=.9)
+    plt.suptitle('Distributions Over the Words for Each Topic', fontsize=16)
+    for idx, (ax, topics) in enumerate(zip(np.ndarray.flatten(axs),top_words_list)):
+        words = [i[0] for i in topics]
+        probs = [i[1] for i in topics]
+
+        y_pos = np.arange(len(words))
+        ax.barh(y_pos, probs, align='center', color = 'navy')
+        ax.set_yticks(y_pos)
+        ax.set_yticklabels(words)
+        ax.invert_yaxis()
+        ax.set_xlabel('P(Words | Topic)')
+        ax.set_title('Topic {}'.format(idx+1))
+    if image_name:
+        plt.savefig('images/{}.png'.format(image_name))
+    plt.show()
+
+# Read In Essays #
 essays = pd.read_csv('project_essays.csv')
-
 documents = (essays['Project Title'] + essays['Project Essay']).values
 
-training_set_size = int(documents.shape[0]*.8)
-test_set_size = int(documents.shape[0]*.2)
 
-training_idx = np.random.randint(documents.shape[0], size=training_set_size)
-test_idx = np.random.randint(documents.shape[0], size=test_set_size)
-training, test = documents[training_idx], documents[test_idx]
-
-
-# idx = (np.random.randint(documents.shape[0], size=training_set_size))
-#
-# #indices = numpy.random.permutation(x.shape[0])
-# training_idx, test_idx = training_idx[:training_set_size], training_idx[training_set_size:]
-# training, test = documents[training_idx,:], documents[test_idx,:]
+# Set Things #
+my_additional_stop_words = ['school']
+my_stop_words = text.ENGLISH_STOP_WORDS.union(my_additional_stop_words)
 
 count_vectorizer = CountVectorizer(analyzer='word',
                                     input='content',
                                     lowercase=True,
-                                    max_df=.95,
+                                    max_df=.9,
                                     max_features=10000,
                                     min_df=1,
                                     ngram_range=(1, 1),
                                     preprocessor=None,
-                                    stop_words='english',
+                                    stop_words=my_stop_words,
                                     strip_accents=None,
                                     token_pattern='(?u)\\b\\w\\w+\\b',
                                     tokenizer=None,
                                     vocabulary=None)
 
-train_tf = count_vectorizer.fit_transform(training)
-test_tf = count_vectorizer.fit_transform(test)
+tf = count_vectorizer.fit_transform(documents)
 
-topics = np.arange(2, 330, 30)
-train_perplexities = []
-test_perplexities = []
+#Uncomment these lines to test your range of topics
+# topic_count = [3]
+#test_perplexities(topic_count, tf)
 
+n_top_words = 5
+n_components = 5
 
-for n in topics:
-    print('working on {} topics.'.format(n))
+# Create an LDA model with a useful number of topics:
+lda = LatentDirichletAllocation(n_components=n_components, max_iter=5,
+                                learning_method='online',
+                                learning_offset=50.,
+                                random_state=0)
 
-    lda = LatentDirichletAllocation(n_components=n, max_iter=5,
-                                    learning_method='online',
-                                    learning_offset=50.,
-                                    random_state=0)
+lda.fit_transform(tf)
 
-    lda.fit(train_tf)
+# Inspect Results #
 
-    train_gamma = lda.transform(train_tf)
-    train_perplexity = lda.perplexity(train_tf, train_gamma)
-
-    test_gamma = lda.transform(test_tf)
-    test_perplexity = lda.perplexity(test_tf, test_gamma)
-
-    train_perplexities.append(train_perplexity)
-    test_perplexities.append(test_perplexity)
-
-
-plot_perplexity(topics, train_perplexities, test_perplexities)
+tf_feature_names = count_vectorizer.get_feature_names()
+top_words = get_top_words(lda, tf_feature_names, n_top_words)
+plot_top_words(1, 5, top_words)
